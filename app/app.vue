16d7220a12602d3e6d9615733991cb93e5d592b2
peynadol <peynadol@gmail.com>
2025-07-21 17:32:30 +0100

    add landing page

--- app/app.vue
+++ app/app.vue
@@ -1,12 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold">
-      My Travel Log
-    </h1>
-    <button class="btn btn-primary">
-      View Map <Icon
-        name="tabler:map"
-      />
-    </button>
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
