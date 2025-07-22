62bc12779aab61c242a3c3a4715710bfd5313d06
peynadol <peynadol@gmail.com>
2025-07-22 17:32:42 +0100

    setup github oauth

---
+++ app/pages/error.vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const route = useRoute();
+const error = route.query.error || "An unexpected error occurred.";
+</script>
+
+<template>
+  <div class="card bg-base-300 container mt-4 min-h-72 text-center mx-auto flex flex-col items-center justify-center gap-4">
+    <div role="alert" class="alert alert-error min-w-1/2 text-center">
+      <span>Oh no! {{ error }}</span>
+    </div>
+    <NuxtLink to="/" class="btn btn-primary mt-4">
+      Home
+      <Icon name="tabler:arrow-left" size="24" />
+    </NuxtLink>
+  </div>
+</template>
--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,3 +1,7 @@
+<script setup lang="ts">
+import AuthButton from "~/components/auth-button.vue";
+</script>
+
 <template>
   <div class="hero bg-base-300 container mx-auto mt-4">
     <div class="hero-content text-center min-h-96">
@@ -9,10 +13,7 @@
           Keep track of your travels and adventures with this simple travel log app.
           Add locations, photos, and notes to create a digital journal of your journeys.
         </p>
-        <button class="btn btn-accent">
-          Sign In With GitHub
-          <Icon name="tabler:brand-github" size="24" />
-        </button>
+        <AuthButton />
       </div>
     </div>
   </div>
