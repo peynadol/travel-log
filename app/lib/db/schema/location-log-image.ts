ad1d411ca5b636b5a707dedff986b74424177a75
peynadol <101970481+peynadol@users.noreply.github.com>
2025-07-22 15:33:38 +0100

    set up better auth (#54)
    Closes #15

--- app/lib/db/schema/location-log-image.ts
+++ app/lib/db/schema/location-log-image.ts
@@ -1,11 +1,13 @@
 import { integer, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+import { user } from "./auth";
 import { locationLog } from "./location-log";
 
 export const locationLogImage = sqliteTable("locationLogImage", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   key: text("key").notNull(),
   locationLogId: integer().notNull().references(() => locationLog.id),
+  userId: integer("user_id").notNull().references(() => user.id),
   createdAt: integer().notNull().$default(() => Date.now()),
   updatedAt: integer().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
 });
--- app/lib/db/schema/location.ts
+++ app/lib/db/schema/location.ts
@@ -1,5 +1,7 @@
 import { integer, real, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+import { user } from "./auth";
+
 export const location = sqliteTable("location", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   name: text("name").notNull(),
@@ -7,6 +9,7 @@ export const location = sqliteTable("location", {
   description: text("description"),
   lat: real("lat").notNull(),
   long: real("long").notNull(),
+  userId: integer("user_id").notNull().references(() => user.id),
   createdAt: integer().notNull().$default(() => Date.now()),
   updatedAt: integer().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
 });
